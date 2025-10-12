c091f9c557ae54d4c66ae65988d7d49a18a9b720
tammert <sebastiaantammer@gmail.com>
2025-10-12 23:08:37 +0200

    move flow listeners to driver (and not device)

--- drivers/climatecontrol/driver.ts
+++ drivers/climatecontrol/driver.ts
@@ -2,6 +2,24 @@ import Homey from 'homey';
 
 class ClimateControlDriver extends Homey.Driver {
     async onInit() {
+        // set Flow card listeners
+        this.homey.flow.getActionCard('set_fan_speed').registerRunListener(async (args, state) => {
+            this.log("set_fan_speed triggered")
+            await args.device.triggerCapabilityListener('fan_speed', args.fan_speed);
+        });
+        this.homey.flow.getActionCard('set_operating_mode').registerRunListener(async (args, state) => {
+            this.log("set_operating_mode triggered")
+            await args.device.triggerCapabilityListener('operating_mode', args.operating_mode);
+        });
+        this.homey.flow.getActionCard('set_vane_mode').registerRunListener(async (args, state) => {
+            this.log("set_vane_mode triggered")
+            await args.device.triggerCapabilityListener('vane_mode', args.vane_mode);
+        });
+        this.homey.flow.getActionCard('set_wide_vane_mode').registerRunListener(async (args, state) => {
+            this.log("set_wide_vane_mode triggered")
+            await args.device.triggerCapabilityListener('wide_vane', args.wide_vane);
+        });
+
         this.log('ClimateControl Driver has been initialized');
     }
 
